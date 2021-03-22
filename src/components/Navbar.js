e9b158e47859460edd946324f0a92d24493143ad
Joseph Perez <hello@josephdanielperez.com>
2021-03-22 14:57:37 -0700

    fundementals

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,51 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function Navbar() {
+
+    const [click, setClick] = useState(false)
+
+    const handleClick = () => {
+        setClick(!click)
+    }
+
+    const closeMobileMenu = () => {
+        setClick(false)
+    }
+
     return (
         <>
             <nav className='navbar'>
                 <div className='navbar-container'>
                     <Link to='/' className='navbar-logo' >
-                        TRVL <i className='fas fa-hand-peace' />
+                        P14 <i className='fas fa-code' />
                     </Link>
+
+                    <div className='menu-icon' onClick={ handleClick }>
+                        <i className={ click ? 'fas fa-times' : 'fas fa-bars' } />
+                    </div>
+
+                    <ul className={ click ? 'nav-menu active' : 'nav-menu' } >
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick={ closeMobileMenu }>
+                                Home
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/services' className='nav-links' onClick={ closeMobileMenu }>
+                                Services
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/products' className='nav-links' onClick={ closeMobileMenu }>
+                                Products
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/about' className='nav-links' onClick={ closeMobileMenu }>
+                                About
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </nav>   
         </>
