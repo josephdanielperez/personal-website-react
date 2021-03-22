a921f1006488d19eb1181478dfb72709e436290b
Joseph Perez <hello@josephdanielperez.com>
2021-03-22 14:42:29 -0700

    fundementals

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
     return (
         <>
             <nav className='navbar'>
                 <div className='navbar-container'>
-                    <Link to='/' className='navbar-logo' >TRVL</Link>
+                    <Link to='/' className='navbar-logo' >
+                        TRVL <i className='fas fa-hand-peace' />
+                    </Link>
                 </div>
             </nav>   
         </>
