fdfd8450cdba06a0d0be1c63680508222b24bc13
Joseph Perez <hello@josephdanielperez.com>
2021-03-23 16:25:34 -0700

    fundementals

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css'
 
 import Navbar from './components/Navbar'
+import About from './components/pages/About'
+import Contact from './components/pages/Contact'
 import Home from './components/pages/Home'
+import Projects from './components/pages/Projects'
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
       <Navbar />
       <Switch>
         <Route path='/' exact component={ Home }/>
+        <Route path='/about' exact component={ About }/>
+        <Route path='/contact' exact component={ Contact }/>
+        <Route path='/projects' exact component={ Projects }/>
       </Switch>
     </Router>
   )
