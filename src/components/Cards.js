d6080e175d8b897bd4036b2c7bb1e47fbd540531
Joseph Perez <hello@josephdanielperez.com>
2021-03-24 15:00:19 -0700

    fundementals

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,6 +3,9 @@ import React from 'react'
 import CardItem from './CardItem'
 import './Cards.css'
 
+import img1 from '../assets/images/img-9.jpeg'
+import img2 from '../assets/images/img-2.jpeg'
+
 function Cards() {
     return (
         <div className='cards'>
@@ -11,8 +14,8 @@ function Cards() {
             <div className='cards__container'>
                 <div className='cards__wrapper'>
                     <ul className='cards__items'>
-                        <CardItem src='/images/img-9.jpeg' text='Explore the hidden waterfall deep inside the Amazon Jungle' label='Adventure' path='/projects' />
-                        <CardItem src='/images/img-2.jpeg' text='Explore the hidden waterfall deep inside the Amazon Jungle' label='Adventure' path='/projects' />
+                        <CardItem src={img1} text='Explore the hidden waterfall deep inside the Amazon Jungle' label='Adventure' path='/projects' />
+                        <CardItem src={img2} text='a beach wow look there are probably sharks in there wowow' label='Adventure' path='/projects' />
                     </ul>
                 </div>
             </div>
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -4,10 +4,12 @@ import { Button } from './Button'
 import '../App.css'
 import './HeroSection.css'
 
+import background from '../assets/videos/video-2.mp4'
+
 function HeroSection() {
     return (
         <div className='hero-container'>
-            <video src='/videos/video-2.mp4' autoPlay loop muted />
+            <video src={background} autoPlay loop muted />
             <h1>Joseph Perez</h1>
             <p>Software Engineer</p>
 
