ec2ff0b80e9715847626ec3e3a1eaf0a1fa8449c
Joseph Perez <hello@josephdanielperez.com>
2021-03-24 15:11:05 -0700

    fundementals

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import '../../App.css'
+import Cards from '../Cards'
 
 function Projects() {
     
     return (
         <>
             <h1 className='projects'>Projects</h1>
+            <Cards />
         </>
     )
 }
