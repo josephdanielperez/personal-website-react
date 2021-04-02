977f0c558038b89f1a7f860f8d786b5332b2651d
Joseph Perez <hello@josephdanielperez.com>
2021-04-01 21:44:54 -0700

    firebase

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import '../../App.css'
 
@@ -10,7 +11,28 @@ function Contact() {
 
             <div className='contact-block'>
                 <h2>All Inquiries</h2>
-                <p>joseph@p14.dev</p>
+                <p><a href='mailto: joseph@p14.dev'>joseph@p14.dev</a></p>
+
+                <section className='social-media'>
+                    <div className='social-media-wrap'>
+                        <div className='social-icons'>
+                            
+                            <Link className='social-icon-link' to='/linkedin' target='_blank' aria-label='LinkedIn'>
+                                <i className='fab fa-linkedin'></i>
+                            </Link>
+
+                            <Link className='social-icon-link' to='/github' target='_blank' aria-label='Github'>
+                                <i className='fab fa-github'></i>
+                            </Link>
+
+                            <Link className='social-icon-link' to='/medium' target='_blank' aria-label='Medium'>
+                                <i className='fab fa-medium'></i>
+                            </Link>
+
+                        </div>
+                    </div>
+                </section>
+
             </div>
         </>
     )
