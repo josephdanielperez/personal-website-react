f20b942c8031cb0adb87bf882a92f7b0a0f6385e
Joseph Perez <hello@josephdanielperez.com>
2021-05-09 19:18:35 -0700

    about sizing and spacing

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -10,9 +10,9 @@ function About() {
             <h1 className='about'>About</h1>
 
             <div className='about-block'>
-                <h1 className='title'>Joseph Perez</h1><br />
+                <h1 className='title'>Joseph Perez</h1>
                 <div className='content'>
-                    <img src={img} alt='profile' className='image' /><br />
+                    <img src={img} alt='profile' className='image' />
 
                     <p>Joseph is an aspiring full-time software engineer based just outside of Seattle, Washington. He has worked as an online content creator partnering up with brands such as Twitch, Astro Gaming, Logitech, Blue Microphones, AMD, and more. Having an interest in all things tech, his curiosity brought him to software engineering. His passion for coding was solidified throughout his time at Flatiron School where he sought to expand his abilities to make a career change after being self-taught as a hobbyist for a couple of years.</p>
                 </div>
