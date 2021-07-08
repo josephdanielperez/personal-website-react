a8fd5d98f2fedbb7d6de096c62fa54e8fe3f636c
Joseph Perez <joseph@p14.dev>
2021-07-08 14:23:59 -0700

    cleaned up about page;

--- src/components/About.js
+++ src/components/About.js
@@ -1,23 +1,22 @@
 import React from 'react'
+import './About.css'
 
-import '../App.css'
 import img from '../assets/images/profile.jpg'
 
 function About() {
     
   return (
-    <>
-      <h1 className='about'>About</h1>
+    <div className='about-container'>
+      <h1 className='about-header'>About</h1>
 
-      <div className='about-block'>
-        <h1 className='title'>Joseph Perez</h1>
-        <div className='content'>
-          <img src={img} alt='profile' className='image' />
-
+      <div className='about-content'>
+        <h1 className='about-title'>Joseph Perez</h1>
+        <div>
+          <img className='about-image' src={img} alt='profile' />
           <p>Joseph is an aspiring full-time software engineer based just outside of Seattle, Washington. He has worked as an online content creator partnering up with brands such as Twitch, Astro Gaming, Logitech, Blue Microphones, AMD, and more. Having an interest in all things tech, his curiosity brought him to software engineering. His passion for coding was solidified throughout his time at Flatiron School where he sought to expand his abilities to make a career change after being self-taught as a hobbyist for a couple of years.</p>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
