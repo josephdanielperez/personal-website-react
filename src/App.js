7dde26f2d75526444879f36f0054f6484f25d923
Joseph Perez <joseph@p14.dev>
2021-07-16 13:35:22 -0700

    styling

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import About from './components/About'
 import Contact from './components/Contact'
 import Home from './components/Home'
 import Projects from './components/Projects'
-import Footer from './components/Footer'
 
 function App() {
   return (
@@ -50,7 +49,6 @@ function App() {
         }} />
 
       </Switch>
-      <Footer />
     </Router>
   )
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FaLinkedin, FaGithub, FaMedium, FaTwitter, FaInstagram, FaYoutube, FaTwitch } from 'react-icons/fa'
 import './Contact.css'
 
 function Contact() {
@@ -7,8 +9,32 @@ function Contact() {
       <h1 className='contact-header'>Contact</h1>
       <div className='contact-content'>
         <section className='email'>
-          <h2>All Inquiries</h2>
+          <h2>Email Address</h2>
           <p><a href='mailto: joseph@p14.dev'>joseph@p14.dev</a></p>
+        </section>
+        <section className='social-media'>
+          <h2>Social Media</h2>
+            <Link className='social-icon-link' to='/linkedin' target='_blank' aria-label='LinkedIn'>
+              <FaLinkedin />
+            </Link>
+            <Link className='social-icon-link' to='/github' target='_blank' aria-label='Github'>
+              <FaGithub />
+            </Link>
+            <Link className='social-icon-link' to='/medium' target='_blank' aria-label='Medium'>
+              <FaMedium />
+            </Link>
+            <Link className='social-icon-link' to='/twitter' target='_blank' aria-label='Twitter'>
+              <FaTwitter />
+            </Link>
+            <Link className='social-icon-link' to='/instagram' target='_blank' aria-label='Instagram'>
+              <FaInstagram />
+            </Link>
+            <Link className='social-icon-link' to='/youtube' target='_blank' aria-label='YouTube'>
+              <FaYoutube />
+            </Link>
+            <Link className='social-icon-link' to='/twitch' target='_blank' aria-label='Twitch'>
+              <FaTwitch />
+            </Link>
         </section>
       </div>
     </div>
